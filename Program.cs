afd4f546ef227bae7098b1ecad1007251610670e
Alenta <mef082024@gmail.com>
2024-10-10 09:47:00 +0200

    MeanSquareError Solved

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace Mean_square_error;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Solution(new int[] {1, 2, 3}, new int[] {4, 5, 6}));
+    }
+
+    public static double Solution(int[] firstArray, int[] secondArray)
+    {
+        // Two int arrays
+        // Compare values, square the difference
+        // Return average of the squared difference pairs
+        List<double> remainders = new();
+        for (int i = 0; i < firstArray.Length; i++)
+        {
+            double result = Math.Pow(Math.Abs(firstArray[i] - secondArray[i]),2);
+            remainders.Add(result);
+        }
+        return Queryable.Average(remainders.AsQueryable());
+    }
+}
